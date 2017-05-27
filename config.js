59599511dded8c339afefcba5e2bc4f616745ee4
Ronen <ronen@users-Air.privatebox>
2017-05-27 13:37:14 +0300

    db added

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+	'secret': 'ilovescotchyscotch',
+	'database': 'mongodb://ronenpi1802:QWeasd123@users-shard-00-00-shmge.mongodb.net:27017,users-shard-00-01-shmge.mongodb.net:27017,users-shard-00-02-shmge.mongodb.net:27017/users?ssl=true&replicaSet=users-shard-0&authSource=admin'
+
+};
